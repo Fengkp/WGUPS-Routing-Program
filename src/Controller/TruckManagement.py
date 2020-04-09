5b398edee2c620b06241eb7cffdb2d8340656092
Feng <parrafk@gmail.com>
2020-04-09 17:35:45 -0400

    Typotyposaddasdwdw

--- src/Controller/TruckManagement.py
+++ src/Controller/TruckManagement.py
@@ -1,26 +1,42 @@
 from src.Controller.PackageAllocation import *
+import datetime
 from src.Model.Truck import Truck
 # Manage truck deliveries
 # Account for and keep track of time of day using truck average speed. Every time the total mileage meets a number
 # divisible by the truck's average speed, an hour has passed.
 # Call PackageAllocation to determine a list of packages with the most optimal route.
 def deliver_packages(package_table, trucks):
-    capacity = 16
+    day = datetime.datetime.today().day
+    month = datetime.datetime.today().month
+    year = datetime.datetime.today().year
+    time_now = datetime.datetime(year, month, day, 8)
     ready_packages = prepare_packages(package_table)
     init_trucks(trucks, ready_packages)
     truck = trucks[0]
     while len(ready_packages) != 0:
-        if truck.get_package_count() == capacity:
+        if truck.get_package_count() == 16:
             go_home(truck)
             package = hub_package(ready_packages)
-            truck.add_delivery(package, package.get_hub_distance())
+            deliver(truck, package, package.get_hub_distance(), time_now, package_table)
         else:
             package = truck.get_deliveries()[-1]
             package, distance = next_package(package, ready_packages)
-            truck.add_delivery(package, distance)
+            deliver(truck, package, distance, time_now, package_table)
         update_ready_packages(package, ready_packages)
         truck = switch_trucks(truck, trucks)
     return total_distance(trucks)
+
+
+def deliver(truck, package, distance, time_now, package_table):
+    time_delivered = add_time(distance, time_now)
+    update_package(package_table, package, 'Delivered', time_delivered)
+    truck.add_delivery(package, distance)
+
+
+def add_time(miles, time_now):
+    test = 3600 / 18 * miles
+    time_now = time_now + datetime.timedelta(seconds=test)
+    return time_now
 
 
 def total_distance(trucks):
@@ -64,8 +80,9 @@ def switch_trucks(truck, trucks):
     return trucks[new_truck_index]
 
 
-def update_package_status(package_table, current_package, new_status):
+def update_package(package_table, current_package, new_status, time_now):
     package_table.get(current_package.get_id()).set_status(new_status)
+    package_table.get(current_package.get_id()).set_time_delivered(time_now)
 
 
 # Returns a list of packages that are ready to be shipped. Packages with a status of 'On route' or 'Delivered'
